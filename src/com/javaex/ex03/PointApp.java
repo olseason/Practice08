4fd189b1caf8f86fddb9692defe42973d130d57c
olseason <sjlee2177@naver.com>
2021-06-15 00:04:42 +0900

    06/14

---
+++ src/com/javaex/ex03/PointApp.java
@@ -0,0 +1,16 @@
+
+package com.javaex.ex03;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+		
+		Point p = new Point(3, 3);
+		
+		System.out.println(p.toString());
+
+	}
+
+}
+
+
